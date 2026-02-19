8f444e7766770b2758a5bd51741ef58300ac32a5
KowneBINKS <118774508+KowneBINKS@users.noreply.github.com>
2026-02-19 10:24:14 +0100

    mise a jour test e2e

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,18 @@
-describe('Home page spec', () => {
-  it('deployed react app to localhost', () => {
+describe('Scénario : Ajout d\'un utilisateur', () => {
+  it('Remplit le formulaire et enregistre un nouvel utilisateur', () => {
+    
     cy.visit('/')
-    cy.contains('Inscription')
+
+    cy.get('input[name="lastName"]').type('Dupont')
+    cy.get('input[name="firstName"]').type('Jean')
+    cy.get('input[name="email"]').type('jean.dupont@test.com')
+    cy.get('input[name="birthDate"]').type('1990-01-01')
+    cy.get('input[name="zipCode"]').type('75000')
+    cy.get('input[name="city"]').type('Paris')
+
+    cy.contains('Envoyer').click()
+
+    cy.contains('Formulaire sauvegardé avec succès !')
+    
   })
 })
