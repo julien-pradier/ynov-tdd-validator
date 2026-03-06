a31b05193693b5d22c57918587cc3bab6c8b3d7d
KowneBINKS <118774508+KowneBINKS@users.noreply.github.com>
2026-03-06 17:07:58 +0100

    tp de ce matin

---
+++ sqlfiles/migration-v002.sql
@@ -0,0 +1,12 @@
+USE ynov_ci;
+
+CREATE TABLE utilisateur
+(
+    id             INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nom            VARCHAR(100),
+    prenom         VARCHAR(100),
+    email          VARCHAR(255),
+    date_naissance DATE
+);
+
+DESCRIBE utilisateur;
